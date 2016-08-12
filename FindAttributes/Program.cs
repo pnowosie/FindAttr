7498fa2fbe3aa33f2b78ef5b7ca539f0b5acce15
Pawel Nowosielski <Pawel.Nowosielski@contractors.roche.com>
2016-08-12 18:02:48 +0200

    That's it :)

---
+++ FindAttributes/Program.cs
@@ -0,0 +1,110 @@
+﻿// Import types from System and System.Reflection
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+//[assembly: AssemblyTitle("Title")]
+
+/// <summary>
+/// Summary
+/// </summary>
+public class FindAttributes
+{
+    static void Main(string[] args)
+    {
+        // Output usage information if necessary
+        if (args.Length == 0)
+            Usage();
+        else if ((args.Length == 1) && (args[0] == "/?"))
+            Usage();
+        else
+        {
+            // Load the assembly
+            string assemblyName = args.First();
+            string attribute = args.Skip(1).FirstOrDefault();
+
+            try
+            {
+                // Attempt to load the named assembly
+                Assembly assembly = Assembly.LoadFrom(assemblyName);
+
+                Console.WriteLine("Assembly attributes for '{0}'...", assembly.GetName().Name);
+                var attributes = ListAttributes(assembly);
+
+                if (attribute == null)
+                    attributes.ToList().ForEach(Console.WriteLine);
+                else if (attribute.StartsWith("/i"))
+                    PrintProperties(assembly);
+                else if (!attributes.Any())
+                    Console.WriteLine("...no attributes");
+                else
+                    PrintAttributeInfo(assembly, attribute);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception thrown loading assembly {0}...", assemblyName);
+                Console.WriteLine();
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    private static void PrintAttributeInfo(Assembly assembly, string substring)
+    {
+        var matches = assembly.GetCustomAttributes(true)
+            .Where(asm => asm.ToString().IndexOf(substring,
+                StringComparison.InvariantCultureIgnoreCase) >= 0)
+            .ToList();
+
+        switch (matches.Count)
+        {
+            case 0:
+                Console.WriteLine("No attribute containing '{0}'.", substring);
+                break;
+            case 1:
+                PrintProperties(matches.Single());
+                break;
+            default:
+                Console.WriteLine("You need to be more specific, matches:");
+                matches.ForEach(m => Console.WriteLine("\t* {0}", m.ToString()));
+                break;
+        }    
+    }
+
+    private static void PrintProperties(object instance)
+    {
+        if (instance == null)
+            throw new ArgumentNullException(nameof(instance));
+
+        Console.WriteLine("Listing properties of {0}", instance.ToString());
+        Dictionary<string, string> properties =
+            (from x in instance.GetType().GetProperties() select x)
+                .ToDictionary(x => x.Name,
+                    x => (x.GetGetMethod().Invoke(instance, null) == null
+                        ? string.Empty
+                        : x.GetGetMethod().Invoke(instance, null).ToString())
+                );
+        foreach (var property in properties)
+        {
+            Console.WriteLine($"{property.Key,15} : {property.Value}");
+        }
+    }
+
+    private static IEnumerable<string> ListAttributes(Assembly assembly)
+    {
+        object[] attributes = assembly.GetCustomAttributes(true);
+
+        return attributes
+            .Select(attr => attr.ToString())
+            .ToList();
+    }
+
+    static void Usage()
+    {
+        Console.WriteLine("Usage:");
+        Console.WriteLine("  FindAttr <Assembly> <Attribute|/info>");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # FindAttr
 List .NET assembly attributes and its values 
+
+## this is really silly project
+Once upon a time I thought it is cool idea to create a lot of custom attributes and decorate assembly with them.
+
+Then I realized that's hard to get value of some attribute out of given assembly file. Maybe I haven't tried hard 
+enogh but even [dotPeek](https://www.jetbrains.com/decompiler/) can't do this.
+
+A then I wrote this tool
+
+## Usage
+
+Listing attributes
+```
+λ .\FindAttr.exe nunit.framework.dll
+
+Assembly attributes for 'nunit.framework'...
+System.Reflection.AssemblyTrademarkAttribute
+System.Reflection.AssemblyInformationalVersionAttribute
+System.Reflection.AssemblyConfigurationAttribute
+System.Security.AllowPartiallyTrustedCallersAttribute
+System.Runtime.CompilerServices.CompilationRelaxationsAttribute
+System.Runtime.CompilerServices.RuntimeCompatibilityAttribute
+System.Reflection.AssemblyKeyFileAttribute
+System.Reflection.AssemblyKeyNameAttribute
+System.Reflection.AssemblyCopyrightAttribute
+System.Reflection.AssemblyDelaySignAttribute
+System.CLSCompliantAttribute
+System.Reflection.AssemblyCompanyAttribute
+System.Reflection.AssemblyProductAttribute
+```
+
+Get value of attribute
+```
+λ .\FindAttr.exe nunit.framework.dll trade
+
+Assembly attributes for 'nunit.framework'...
+Listing properties of System.Reflection.AssemblyTrademarkAttribute
+      Trademark : NUnit is a trademark of NUnit.org
+         TypeId : System.Reflection.AssemblyTrademarkAttribute
+```
+
+Get properties of assembly
+
+```
+λ .\FindAttr.exe nunit.framework.dll /info
+
+Assembly attributes for 'nunit.framework'...
+Listing properties of nunit.framework, Version=2.6.2.12296, Culture=neutral, PublicKeyToken=96d09a1eb7f44a77
+       CodeBase : file:///C:/Projects/Roche/UIA/Roche.UIA/packages/NUnit.2.6.2/lib/nunit.framework.dll
+       FullName : nunit.framework, Version=2.6.2.12296, Culture=neutral, PublicKeyToken=96d09a1eb7f44a77
+     EntryPoint :
+   DefinedTypes : System.RuntimeType[]
+       Evidence : System.Security.Policy.Evidence
+  PermissionSet : <PermissionSet class="System.Security.PermissionSet"
+version="1"
+Unrestricted="true"/>
+ ...
+```
+
+Thanks for reading!
